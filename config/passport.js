c6ae669ebf7c0e715a99dcfa6a7a4cf44f803c48
mscreationweb <mscreation361@gmail.com>
2025-10-20 12:37:23 +0530

    passport file changes

--- config/passport.js
+++ config/passport.js
@@ -33,7 +33,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      callbackURL: "/api/auth/google/callback",
       scope: ["profile", "email"],
     },
     async (accessToken, refreshToken, profile, done) => {
